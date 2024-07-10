2057ca85d7510007476b1cfad5ebc1a0ae1861c4
prabala <radhakrishnaprabala8@gmail.com>
2024-07-10 12:15:23 +0530

    main.tf aws-ssm ver1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "parameter1" {
-  name  = "test1"     # system manger in aws
+resource "aws_ssm_parameter" "parameter2" {
+  name  = "test2"     # system manger in aws
   type  = "String"    #["String" "StringList" "SecureString"]
   value = "hello rk"  # any text
   # key_id = "8dae0259-1d22-47ef-a533-2cf7a81cb5c5" # kms have to create in aws and in ansible this not working
