3f57e213b721f2b7d2e08011a29891397f9dfa67
prabala <radhakrishnaprabala8@gmail.com>
2024-12-03 23:30:39 +0530

    main.tfvars v1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameter6" {
   name  = var.parameter[count.index].name   # keep folder name   # system manger in aws
   value = var.parameter[count.index].value  # any text
   type  = "String"                          # ["String" "StringList" "SecureString"]
-  key_id = "8dae0259-1d22-47ef-a533-2cf7a81cb5c5" # kms have to create in aws and in ansible this not working
+  key_id = "6f8c51d4-28a6-4003-a9da-4c25b067e30b" # kms have to create in aws and in ansible this not working
 }
 
 resource "aws_ssm_parameter" "password" {
@@ -11,5 +11,5 @@ resource "aws_ssm_parameter" "password" {
   name  = var.password[count.index].name   # keep folder name   # system manger in aws
   value = var.password[count.index].value  # any text
   type  = "SecureString"                          # ["String" "StringList" "SecureString"]
-  key_id = "8dae0259-1d22-47ef-a533-2cf7a81cb5c5" # kms have to create in aws and in ansible this not working
+  key_id = "6f8c51d4-28a6-4003-a9da-4c25b067e30b" # kms have to create in aws and in ansible this not working
 }
